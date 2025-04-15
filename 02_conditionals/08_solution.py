f557a6fa6dfa670bf58a12353d52fd3f0a45aced
heygautam2001 <gautamfrankestein@gmail.com>
2025-04-15 20:16:20 +0530

    Adds more in conditionals

--- 02_conditionals/08_solution.py
+++ 02_conditionals/08_solution.py
@@ -1,1 +1,11 @@
 # password checker : easy  , medium , strong
+
+noOfDigitInPassword = int(input("Enter you password lenght"))
+
+if noOfDigitInPassword < 8:
+    print("Your passowrd is easy to guess : ")
+elif noOfDigitInPassword >= 8 and noOfDigitInPassword < 12:
+    print("Your password is should not guesed easily")
+else:
+    print("Your password is hard to guess")
+
--- 02_conditionals/09_solution.py
+++ 02_conditionals/09_solution.py
@@ -1,1 +1,10 @@
 #verify year : normal or leap
+
+Year = int(input("Enter year : \n"))
+
+if (Year % 4 == 0 or Year % 400 ==0 ) and Year % 100 != 0:
+    print("You entered a leap year" , Year)
+
+else:
+    print("You entered a non leap year " ,Year)
+
